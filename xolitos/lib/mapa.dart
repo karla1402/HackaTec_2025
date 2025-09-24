dc2c06f536c495367a3dc306160e89b28747e71b
Edjajim <edyjarajimenez@gmail.com>
2025-09-24 16:48:49 -0600

    Web arreglado

--- xolitos/lib/mapa.dart
+++ xolitos/lib/mapa.dart
@@ -3,7 +3,7 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 
 class MapScreen extends StatefulWidget {
-  const MapScreen({Key? key}) : super(key: key);
+  const MapScreen({super.key});
 
   @override
   _MapScreenState createState() => _MapScreenState();
